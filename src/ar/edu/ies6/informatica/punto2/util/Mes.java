81deff1d07bc05233415ea086a5ee34271d6eb4f
santy <santy@DESKTOP-D4CQLD3>
2024-09-19 02:57:33 -0300

    Inicializacion de proyecto - Yebara Santiago Nahuel

---
+++ src/ar/edu/ies6/informatica/punto2/util/Mes.java
@@ -0,0 +1,16 @@
+package ar.edu.ies6.informatica.punto2.util;
+
+public enum Mes {
+	ENERO,
+	FEBRERO,
+	MARZO,
+	ABRIL,
+	MAYO,
+	JUNIO,
+	JULIO,
+	AGOSTO,
+	SEPTIEMBRE,
+	OCTUBRE,
+	NOVIEMBRE,
+	DICIEMBRE,
+}
---
+++ src/ar/edu/ies6/informatica/punto3/util/Region.java
@@ -0,0 +1,9 @@
+package ar.edu.ies6.informatica.punto3.util;
+
+public enum Region {
+	NOA,
+	NEA,
+	CENTRO,
+	CUYO,
+	PATAGONIA,
+}
